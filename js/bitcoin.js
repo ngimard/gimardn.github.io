1f788fcd25f557f54e97349874b39bad14e13cb7
Nakorey Gimard <gwadagal97.1@hotmail.fr>
2017-04-25 20:47:25 +0200

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -1,1 +1,37 @@
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/blockchaininfo",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
 
+		success : function(data) {
+			$('#blockchaininfo_blocks').append(data.blocks);
+		},
+
+		error : function(xhr, status, err) {
+			$('#blockchaininfo_blocks').append(err+" N/A");
+		}
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#info_connections').append(data.connections);
+		},
+
+		error : function(xhr, status, err) {
+			$('#info_connections').append(err+" N/A");
+		}
+	});
+});
