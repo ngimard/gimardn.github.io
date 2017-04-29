4686835e33e8d974bc0d611d3e718698855addc0
Nakorey Gimard <gwadagal97.1@hotmail.fr>
2017-04-29 12:05:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,22 +37,17 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "https://bitcoin.mubiz.com/mininginfo", true);
 xmlhttp.send();
 }
-$(document).ready(function() {
-	$.ajax({
-		url : "https://api.blockcypher.com/v1/btc/main",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#blockchain').append(data.blocks);
-		},
-
-		error : function(xhr, status, err) {
-			$('#blockchain').append(err+" N/A");
-		}
-	});
-	});
+function blockchain()
+{
+    var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("blockchain").innerHTML = jsonPretty;
+    }
 };
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
+}
+
