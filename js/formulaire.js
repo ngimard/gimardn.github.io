67cbdf2ca02c3c99e0981387f7d773800c963fd6
Nakorey Gimard <gwadagal97.1@hotmail.fr>
2017-04-29 12:44:11 +0200

    Update formulaire.js

--- js/formulaire.js
+++ js/formulaire.js
@@ -9,6 +9,20 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("adress").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main/addrs/1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD/balance", true);
+xmlhttp.open("GET", "https://bitcoin.mubiz.com/address/ bitcoin_address /", true);
+xmlhttp.send();
+}
+function transactionAPI()
+{
+  var isAddress = /^[0-9a-zA-Z]{34}$/.test(data);
+   var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("transaction").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://bitcoin.mubiz.com/transaction/ bitcoin_trx /", true);
 xmlhttp.send();
 }
