b02e9a3616699df5724d9b5c6cfe727ff123a9b5
Nakorey Gimard <gwadagal97.1@hotmail.fr>
2017-04-29 13:00:17 +0200

    Update formulaire.js

--- js/formulaire.js
+++ js/formulaire.js
@@ -3,7 +3,7 @@ function output(inp) {
 }
 
 function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&').replace(//g, '>');
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
         var cls = 'number';
         if (/^"/.test(match)) {
@@ -21,6 +21,8 @@ function syntaxHighlight(json) {
     });
 }
 
-var str = JSON.stringify(id, undefined, 4);
+var obj = {a:1, 'b':'foo', c:[false,'false',null, 'null', {d:{e:1.3e5,f:'1.3e5'}}]};
+var str = JSON.stringify(obj, undefined, 4);
+
 output(str);
 output(syntaxHighlight(str));
